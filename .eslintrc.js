a76b84c1557e7b671fe61cb8754c344feb160212
tormozz48 <tormozz48@gmail.com>
2017-11-16 17:31:21 +0300

    Add some advanced eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
     extends: 'gemini-testing',
-    root: true
+    root: true,
+    plugins: [
+        'react'
+    ],
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+            experimentalObjectRestSpread: true
+        }
+    },
+    rules: {
+        'react/jsx-uses-react': 2,
+        'react/jsx-uses-vars': 2,
+        'react/jsx-no-undef': [2, { allowGlobals: true }]
+    }
 };
