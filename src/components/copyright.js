5de3fd144af728b8f5b377411c4067fe01ca2915
tormozz48 <tormozz48@gmail.com>
2024-05-09 20:59:26 +0400

    [fix] Fix copyright year

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Glyphicon from 'react-bootstrap/es/Glyphicon.js';
 
 export default () => {
+  const year = new Date().getFullYear();
   return (
     <div className="copyright">
       <a className="copyright__author" href="mailto:andrey.kuznetsov48@yandex.ru">
         Andrey Kuznetsov
       </a>
       <Glyphicon glyph="copyright-mark"/>
-      <span className="copyright__year">2021</span>
+      <span className="copyright__year">{year}</span>
     </div>
   );
 };
